edbaa64c610a648f86e70ab8fef6941b8fd2b62d
Shubham Jangid <53791298+shubhamjangid532@users.noreply.github.com>
2020-07-28 14:49:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-  echo "Hello I'm Shubham Jangid";
-  echo "This is my task 6 Groovy Automation";
+  echo " Hello I'm Shubham Jangid \r\n";
+  echo "\r\n This is my task 6 Groovy Automation";
 ?>
